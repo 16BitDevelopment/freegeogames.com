16e6e06195ee2e8840c9137c5cad1da932bfb256
16bitdevelopment <16bitdevelopment@gmail.com>
2024-09-18 13:07:48 +1000

    Update

--- play.js
+++ play.js
@@ -1,5 +1,10 @@
 const loadingScreen = document.getElementById("load-screen");
+const activeGame = document.getElementById("active-game");
 
 function gameLoaded() {
     loadingScreen.classList.add("loaded")
+}
+
+function fullscreen() {
+    activeGame.classList.toggle("fullscreen");
 }

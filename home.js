de854d071b46c26a4cdeebe4c6a9652e060dd5c5
16bitdevelopment <16bitdevelopment@gmail.com>
2024-09-22 06:03:19 +1000

    Home Page 50% Done

---
+++ home.js
@@ -0,0 +1,35 @@
+const popularGames = document.getElementById("popular-games");
+const dots = document.getElementById("active-game-display");
+
+let activePopularGame = 1;
+
+dotsInit();
+updateActiveGame();
+
+function dotsInit() {
+    for(let i = 0; i < Array.from(popularGames.children).length; i += 1) {
+        const el = document.createElement("div");
+        el.classList.add("dot")
+        dots.append(el);
+    }
+}
+
+function updateActiveGame() {
+    if (activePopularGame > Array.from(popularGames.children).length) {
+        activePopularGame = 1
+    } else if (activePopularGame < 1) {
+        activePopularGame = Array.from(popularGames.children).length;
+    }
+
+    popularGames.setAttribute("data-active-game", activePopularGame);
+
+    Array.from(popularGames.children).forEach((el) => {
+        el.classList.remove("active");
+    });
+    popularGames.children[activePopularGame - 1].classList.add("active");
+
+    Array.from(dots.children).forEach(dot => {
+        dot.classList.remove("active");
+    });
+    dots.children[activePopularGame - 1].classList.add("active");
+}

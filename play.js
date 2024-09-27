80f1a20990f9f5ab563ffe06eb7d3e9a72e6b39d
16bitdevelopment <16bitdevelopment@gmail.com>
2024-09-28 00:21:55 +1000

    Fullscreen Safari And IE11 (idk) Compatible

--- play.js
+++ play.js
@@ -9,9 +9,21 @@ function gameLoaded() {
 
 function fullscreen() {
     if (document.fullscreenElement) {
-        document.exitFullscreen();
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+        }
     } else {
-        activeGame.requestFullscreen();
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
+        }
     }
 }
 

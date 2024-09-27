15f6aea50eb06c8103c19f3b620f927e90afe40e
16bitdevelopment <16bitdevelopment@gmail.com>
2024-09-28 03:56:34 +1000

    Fullscreen Bug (Actually) Fixed

--- play.js
+++ play.js
@@ -17,12 +17,12 @@ function fullscreen() {
             document.msExitFullscreen();
         }
     } else {
-        if (elem.requestFullscreen) {
-            elem.requestFullscreen();
-        } else if (elem.webkitRequestFullscreen) { /* Safari */
-            elem.webkitRequestFullscreen();
-        } else if (elem.msRequestFullscreen) { /* IE11 */
-            elem.msRequestFullscreen();
+        if (activeGame.requestFullscreen) {
+            activeGame.requestFullscreen();
+        } else if (activeGame.webkitRequestFullscreen) { /* Safari */
+            activeGame.webkitRequestFullscreen();
+        } else if (activeGame.msRequestFullscreen) { /* IE11 */
+            activeGame.msRequestFullscreen();
         }
     }
 }

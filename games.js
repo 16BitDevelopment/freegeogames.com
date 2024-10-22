39c594d39c262142d8517d6e73d6f500c1004ebf
16bitdevelopment <16bitdevelopment@gmail.com>
2024-10-23 09:55:51 +1100

    Where Taken Game Added

--- games.js
+++ games.js
@@ -1,4 +1,4 @@
-let games = ["globle", "tradle", "travle", "flagle", "worldle", "statele", "globle-capitals", "geoguessr", "geo-quiz"];
+let games = ["globle", "tradle", "travle", "flagle", "worldle", "statele", "globle-capitals", "geoguessr", "geo-quiz", "where-taken"];
 
 // load games
 
